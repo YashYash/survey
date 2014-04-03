3ea61d5e915811f97a23f8ed76f9bd801100896f
YashYash <yash.saxena1217@gmail.com>
2014-04-03 16:11:12 -0700

    founders-network-templates

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "home.html")
+
+
+def billing(request):
+    return render(request, "network-approvals(1).html")
--- survey_monkey/urls.py
+++ survey_monkey/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'survey.views.index', name='index'),
+    url(r'^billing$', 'survey.views.billing', name='billing'),
 )
